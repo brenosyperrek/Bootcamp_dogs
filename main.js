780c4562b5189e0dcab0519f1c9dc9c5706f9fac
brenosyperrek <brenosyperrek@gmail.com>
2023-03-08 17:26:12 -0300

    Comitando acessos a api

--- main.js
+++ main.js
@@ -1,9 +1,28 @@
 async function start() {
-  const response = await fetch("https://dog.ceo/api/breeds/list/all");
-  const data = await response.json();
+  const response = await fetch("https://dog.ceo/api/breeds/list/all")
+  const data = await response.json()
   createBreedList(data.message)
 }
 
-function createBreedList(breedList) {
-  document.getElementById("breed").innerHTML = "hey"
+start()
+
+function createBreedList(breedList){
+  document.getElementById("breed").innerHTML = `
+  <select onchange="loadByBreed(this.value)">
+    <option>Escolha uma raça de cachorro</option>
+    ${Object.keys(breedList).map(function (breed){
+      return `<option>${breed}</option>`
+    }).join('')}
+  </select>
+  `
+}
+
+async function loadByBreed(breed){
+  if (breed != "Escolha uma raça de cachorro"){
+    const response = await fetch(`https://dog.ceo/api/breed/${breed}/images`)
+    const data = await response.json()
+    console.log(data)
+  }
+  
+  
 }
